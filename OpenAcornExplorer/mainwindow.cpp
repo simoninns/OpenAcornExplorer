6038a3c0e160e2a6e833e2275706e373f56b1242
Simon Inns <simon.inns@gmail.com>
2018-04-03 18:08:20 +0200

    Added basic QT project and source outline

---
+++ OpenAcornExplorer/mainwindow.cpp
@@ -0,0 +1,40 @@
+/************************************************************************
+
+    mainwindow.cpp
+
+    OpenAcornExplorer - Acorn 8-bit and 32-bit disc image manipulation
+    Copyright (C) 2018 Simon Inns
+
+    This file is part of OpenAcornExplorer.
+
+    OpenAcornExplorer is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ OpenAcornExplorer/mainwindow.h
@@ -0,0 +1,48 @@
+/************************************************************************
+
+    mainwindow.h
+
+    OpenAcornExplorer - Acorn 8-bit and 32-bit disc image manipulation
+    Copyright (C) 2018 Simon Inns
+
+    This file is part of OpenAcornExplorer.
+
+    OpenAcornExplorer is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Email: simon.inns@gmail.com
+
+************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
